b4f93ffe90c8863e65df7bd981844b5e19e26cc2
JJJ4065 <darknessrisesgamer999@gmail.com>
2026-02-10 10:01:11 +0700

    Initial commit

--- copy_of_connect_linkedin_with_cookie.py
+++ copy_of_connect_linkedin_with_cookie.py
@@ -135,7 +135,7 @@ def get_driver():
     options.add_argument('--no-sandbox')
     options.add_argument("--disable-dev-shm-usage")
     options.add_argument('--disable-gpu')
-    #options.add_argument('--headless=new')
+    options.add_argument('--headless=new')
     options.add_argument("--window-size=1920,1200")
     
     # 3. CHỐNG PHÁT HIỆN BOT (Stealth Mode)
